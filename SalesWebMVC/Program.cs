44d06bb0eb5461f23ec9b54a51b6bfa76ec9f9d4
br-candido <brunorogeriocandido@gmail.com>
2023-03-13 16:43:40 -0300

    Adding SellersController

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -6,6 +6,8 @@ var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found."), serverVersion));
 
+builder.Services.AddScoped<SeedingService, SeedingService>();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -17,6 +19,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var sampleService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+    sampleService.Seed();
 }
 
 app.UseHttpsRedirection();
