f416d89533a819d00b24b96e7c0e821a00d9e8e7
br-candido <brunorogeriocandido@gmail.com>
2023-03-13 18:05:38 -0300

    Foreign Key not null

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,4 +1,6 @@
-﻿namespace SalesWebMVC.Models
+﻿using NuGet.Protocol.Core.Types;
+
+namespace SalesWebMVC.Models
 {
     public class Seller
     {
@@ -9,6 +11,7 @@
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
